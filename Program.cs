257677e70d1592bd96d7b2da360b924a89042bf1
Samuel Karlström <sam.karlstrm@gmail.com>
2020-11-13 16:37:08 +0100

    Uppgift 1

--- Program.cs
+++ Program.cs
@@ -17,22 +17,9 @@ namespace progmet_assignment2
     {
         static void Main(string[] args)
         {
-            List<Person> Dict = new List<Person>();
+            List<Person> dict = new List<Person>();
 
-            Console.Write("Laddar adresslistan ... ");
-            using (StreamReader fileStream = new StreamReader(@"C:\Users\samka\addressbook.lis"))
-            {
-                while (fileStream.Peek() >= 0)
-                {
-                    string line = fileStream.ReadLine();
-                    // Console.WriteLine(line);
-                    string[] word = line.Split('#');
-                    // Console.WriteLine("{0}, {1}, {2}, {3}", word[0], word[1], word[2], word[3]);
-                    Person P = new Person(word[0], word[1], word[2], word[3]);
-                    Dict.Add(P);
-                }
-            }
-            Console.WriteLine("klart!");
+            LoadList(dict);
 
             Console.WriteLine("Hej och välkommen till adresslistan");
             Console.WriteLine("Skriv 'sluta' för att sluta!");
@@ -56,16 +43,16 @@ namespace progmet_assignment2
                     string telefon = Console.ReadLine();
                     Console.Write("  4. ange email:   ");
                     string email = Console.ReadLine();
-                    Dict.Add(new Person(name, adress, telefon, email));
+                    dict.Add(new Person(name, adress, telefon, email));
                 }
                 else if (command == "ta bort")
                 {
                     Console.Write("Vem vill du ta bort (ange namn): ");
                     string villTaBort = Console.ReadLine();
                     int found = -1;
-                    for (int i = 0; i < Dict.Count(); i++)
+                    for (int i = 0; i < dict.Count(); i++)
                     {
-                        if (Dict[i].namn == villTaBort) found = i;
+                        if (dict[i].namn == villTaBort) found = i;
                     }
                     if (found == -1)
                     {
@@ -73,14 +60,14 @@ namespace progmet_assignment2
                     }
                     else
                     {
-                        Dict.RemoveAt(found);
+                        dict.RemoveAt(found);
                     }
                 }
                 else if (command == "visa")
                 {
-                    for (int i = 0; i < Dict.Count(); i++)
+                    for (int i = 0; i < dict.Count(); i++)
                     {
-                        Person P = Dict[i];
+                        Person P = dict[i];
                         Console.WriteLine("{0}, {1}, {2}, {3}", P.namn, P.adress, P.telefon, P.email);
                     }
                 }
@@ -89,9 +76,9 @@ namespace progmet_assignment2
                     Console.Write("Vem vill du ändra (ange namn): ");
                     string villÄndra = Console.ReadLine();
                     int found = -1;
-                    for (int i = 0; i < Dict.Count(); i++)
+                    for (int i = 0; i < dict.Count(); i++)
                     {
-                        if (Dict[i].namn == villÄndra) found = i;
+                        if (dict[i].namn == villÄndra) found = i;
                     }
                     if (found == -1)
                     {
@@ -105,10 +92,10 @@ namespace progmet_assignment2
                         string nyttVärde = Console.ReadLine();
                         switch (fältAttÄndra)
                         {
-                            case "namn": Dict[found].namn = nyttVärde; break;
-                            case "adress": Dict[found].adress = nyttVärde; break;
-                            case "telefon": Dict[found].telefon = nyttVärde; break;
-                            case "email": Dict[found].email = nyttVärde; break;
+                            case "namn": dict[found].namn = nyttVärde; break;
+                            case "adress": dict[found].adress = nyttVärde; break;
+                            case "telefon": dict[found].telefon = nyttVärde; break;
+                            case "email": dict[found].email = nyttVärde; break;
                             default: break;
                         }
                     }
@@ -118,6 +105,24 @@ namespace progmet_assignment2
                     Console.WriteLine("Okänt kommando: {0}", command);
                 }
             } while (command != "sluta");
+        }
+
+        static void LoadList(List<Person> dict)
+        {
+            Console.Write("Laddar adresslistan ... ");
+            using (StreamReader fileStream = new StreamReader(@"C:\Users\samka\addressbook.lis"))
+            {
+                while (fileStream.Peek() >= 0)
+                {
+                    string line = fileStream.ReadLine();
+                    // Console.WriteLine(line);
+                    string[] word = line.Split('#');
+                    // Console.WriteLine("{0}, {1}, {2}, {3}", word[0], word[1], word[2], word[3]);
+                    Person P = new Person(word[0], word[1], word[2], word[3]);
+                    dict.Add(P);
+                }
+            }
+            Console.WriteLine("klart!");
         }
     }
 }
