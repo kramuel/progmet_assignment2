2d797f803659904ffa0c4593ed8842a3a461c0d2
Samuel Karlström <sam.karlstrm@gmail.com>
2020-11-13 18:20:52 +0100

    Uppgift 7-10

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 
 namespace progmet_assignment2
 {
+    /* CLASS: Person
+     * PURPOSE: Is used in a List (List<Person>) to contain all Persons in a addressbook.
+     */
     class Person
     {
         public string name, address, phone, email;
@@ -12,6 +15,12 @@ namespace progmet_assignment2
         {
             name = N; address = A; phone = T; email = E;
         }
+
+        /* METHOD: Person() (static)
+         * PURPOSE: Uses Console.Readline() and Console.Write() to ask and get input from user. Uses input to assign class-variables.
+         * PARAMETERS: no parameters used
+         * RETURN VALUE: 
+         */
 
         public Person()
         {
@@ -24,14 +33,24 @@ namespace progmet_assignment2
             phone = Console.ReadLine();
             Console.Write("  4. ange email:   ");
             email = Console.ReadLine();
-            //dict.Add(new Person(name, adress, telefon, email));
         }
 
+        /* METHOD: Print (static)
+         * PURPOSE: Outputs name, address, phone, and email using string interpolation and Console.WriteLine)=
+         * PARAMETERS: no parameters used
+         * RETURN VALUE: void method
+         */
         public void Print()
         {
             Console.WriteLine($"{name}, {address}, {phone}, {email}");
         }
 
+        /* METHOD: Modify (static)
+         * PURPOSE: Uses a Switch-Case to appoint newValue to correct object-variable.
+         * PARAMETERS: string sectionToModify: contains "name" || "adress" || "telefon" || "email" depending on what section user wants to modify
+         *             string newValue: contains the new value that replaces the saved value.
+         * RETURN VALUE: returvärdets innebörd
+         */
         public void Modify(string sectionToModify, string newValue)
         {
             switch (sectionToModify)
@@ -66,7 +85,7 @@ namespace progmet_assignment2
                 else if (command == "ny")
                 {
                     dict.Add(new Person());
-                    
+
                 }
                 else if (command == "ta bort")
                 {
@@ -90,7 +109,11 @@ namespace progmet_assignment2
             } while (command != "sluta");
         }
 
-
+        /* METHOD: LoadList (static)
+         * PURPOSE: Uses StreamReader to open and read a textfile. Adds new Persons to List<Person>.
+         * PARAMETERS: List<Person> dict, the list containing all Persons in the addresslist.
+         * RETURN VALUE: void method
+         */
         static void LoadList(List<Person> dict)
         {
             Console.Write("Laddar adresslistan ... ");
@@ -108,39 +131,36 @@ namespace progmet_assignment2
             Console.WriteLine("klart!");
         }
 
+        /* METHOD: RemovePerson (static)
+         * PURPOSE: Searches list for same name as input using List.Find(). Removes that Person using List.Remove();
+         * PARAMETERS: List<Person> dict, the list containing all Persons in the addresslist.
+         * RETURN VALUE: void method
+         */
         static void RemovePerson(List<Person> dict)
         {
             Console.Write("Vem vill du ta bort (ange namn): ");
             string toRemove = Console.ReadLine();
-            int found = -1;
-            for (int i = 0; i < dict.Count(); i++)
-            {
-                if (dict[i].name == toRemove) found = i;
-            }
-            if (found == -1)
-            {
-                Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", toRemove);
-            }
+            if (dict.Remove(dict.Find(x => x.name == toRemove)))
+                return;
             else
-            {
-                dict.RemoveAt(found);
-            }
+                Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", toRemove);
+
         }
 
+        /* METHOD: ModifyPerson (static)
+         * PURPOSE: Using input to search for matchning name with List.Find(). Calls Person.Modify method to modify a section of that Person.
+         * PARAMETERS: List<Person> dict, the list containing all Persons in the addresslist.
+         * RETURN VALUE: void method
+         */
         static void ModifyPerson(List<Person> dict)
         {
             Console.Write("Vem vill du ändra (ange namn): ");
             string toModify = Console.ReadLine();
-            int found = -1;
-            for (int i = 0; i < dict.Count(); i++)
-            {
-                if (dict[i].name == toModify) found = i;
-            }
 
-            if (found == -1)
-            {
+            Person foundPerson = dict.Find(x => x.name == toModify);
+
+            if (foundPerson == null)
                 Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", toModify);
-            }
             else
             {
                 Console.Write("Vad vill du ändra (namn, adress, telefon eller email): ");
@@ -148,8 +168,9 @@ namespace progmet_assignment2
                 Console.Write("Vad vill du ändra {0} på {1} till: ", sectionToModify, toModify);
                 string newValue = Console.ReadLine();
 
-                dict[found].Modify(sectionToModify, newValue);
+                foundPerson.Modify(sectionToModify, newValue);
             }
+
         }
     }
 }
